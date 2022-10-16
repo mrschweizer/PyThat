487e7376a1d6c95387cb06ea068429023e4e0330
Matthias <mschweiz@rhrk.uni-kl.de>
2022-10-16 22:59:05 +0200

    Update API reference to reflect changes in 0.0.39

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Matthias R. Schweizer'
 author = 'Matthias R. Schweizer'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.33'
+release = '0.0.39'
 
 
 # -- General configuration ---------------------------------------------------
