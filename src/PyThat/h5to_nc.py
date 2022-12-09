ff44044754c0814e40448062ac5790524f2f77c3
Matthias <mschweiz@rhrk.uni-kl.de>
2022-12-09 13:58:41 +0100

    Add dask dependency

--- src/PyThat/h5to_nc.py
+++ src/PyThat/h5to_nc.py
@@ -14,6 +14,7 @@ class MeasurementTree:
         """
         :param filepath: r-string that points to h5 file
         :param index: optional: tuple that describes group number and group internal number
+        :param chunk: None will skip chunking before saving, True will set chunks to 'auto', dict will forward dict to xarray.Dataset.chunk call
         """
         self.filepath = filepath
         self.path = pl.Path(filepath).absolute()
