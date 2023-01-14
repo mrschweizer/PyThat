52b8d2ec82d12598ef5b13a47821b6b0513df1c1
Matthias <mschweiz@rhrk.uni-kl.de>
2023-01-14 23:18:19 +0100

    Add first functions in the dB module

--- src/PyThat/helpers/dB.py
+++ src/PyThat/helpers/dB.py
@@ -1,7 +1,14 @@
 import numpy as np
+import xarray
 
 def dB_to_lin(x):
     return np.power(10, x/10)
 
+xarray.DataArray.dB_to_lin = dB_to_lin
+xarray.Dataset.dB_to_lin = dB_to_lin
+
 def lin_to_dB(x, ref=1):
     return 10*np.log10(x/ref)
+
+xarray.DataArray.lin_to_dB = lin_to_dB
+xarray.Dataset.lin_to_dB = lin_to_dB
