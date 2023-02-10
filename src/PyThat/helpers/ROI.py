5b74e87fccd08147509745cf94c7b8382b526803
Matthias <mschweiz@rhrk.uni-kl.de>
2023-02-10 13:24:16 +0100

    postpone changes in ROI module

--- src/PyThat/helpers/ROI.py
+++ src/PyThat/helpers/ROI.py
@@ -1,28 +1,18 @@
 import xarray as xr
+from numpy import arange
 
 
-def monotone_divide(x: xr.DataArray or xr.Dataset, dim=None):
-    if dim is None:
-        raise ValueError('dim needs to be specified.')
-    dx = x.diff(dim)
-    print(x)
-    incr = dx > 0
-    decr = dx < 0
-    _connected_(incr)
-    print(incr, incr.shape)
-    print(decr, decr.shape)
+# def monotone_divide(x: xr.DataArray or xr.Dataset, dim=None):
+#     if dim is None:
+#         raise ValueError('dim needs to be specified.')
+#     dx = x.diff(dim)
+#     print(x)
+#     incr = dx > 0
+#     right_end = (incr != incr.shift({dim: -1})).pad({dim: (1, 0)}, constant_values=False)
+#     # left_end = (incr != incr.shift({dim: 1})).pad({dim: (1, 0)}, constant_values=False)
+#     # print(x.where(arange(x.sizes[dim]), drop=True))
+#     print(x[dim].where(right_end, drop=True))
 
-def _connected_(x):
-    for i, y in enumerate(x):
-        print('____')
-        print(i, y)
-        y0 = i
-        if y:
-
-
-
-
-
-if __name__ == '__main__':
-    x = xr.DataArray([1, 2, 4, 4, 3], dims=('y'))
-    monotone_divide(x, 'y')
+# if __name__ == '__main__':
+#     x = xr.DataArray(list(range(6))+[5, 5, 5]+list(range(4, 0, -1))+list(range(3, 9)), dims=('y'))
+#     monotone_divide(x, 'y')
