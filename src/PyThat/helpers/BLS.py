46d377f5c73852f10b41f179609785179fd63a67
Matthias <mschweiz@rhrk.uni-kl.de>
2023-02-10 13:24:32 +0100

    add mask_FO function

--- src/PyThat/helpers/BLS.py
+++ src/PyThat/helpers/BLS.py
@@ -1,7 +1,7 @@
 import xarray
 
 
-def norm_on_FO(x, name='Frequency' ,f_max=2):
+def norm_on_FO(x, dim=None, f_max=2):
     """
     Normalize a given dataset x on the average of the FO signal around zero frequency.
     :param x: xarray object
@@ -9,7 +9,21 @@ def norm_on_FO(x, name='Frequency' ,f_max=2):
     :param f_max: specifies the width of the frequency area over which is to be normalized
     :return: normalized xarray object
     """
-    return x/x.where(abs(x[name]) < f_max).mean(name).squeeze()
+    return x/x.where(abs(x[dim]) < f_max).mean(dim).squeeze()
+
+
+def mask_FO(x, dim=None, f_max=2):
+    """
+    Mask an array around the center frequency.
+    :param x: xarray object
+    :param name: Name of the Frequency coordinate axis along which the frequency is to be Masked.
+    :param f_max: specifies the width of the frequency area over which is to be normalized
+    :return: xarray object where the frequencies around f=0 are set to nan
+    """
+    return x.where(abs(x[dim]) >= f_max)
+
 
 xarray.DataArray.norm_on_FO = norm_on_FO
 xarray.Dataset.norm_on_FO = norm_on_FO
+xarray.DataArray.mask_FO = mask_FO
+xarray.Dataset.mask_FO = mask_FO
