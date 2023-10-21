c04202c476a9520670a75bb6d8cae46dba3fe609
Matthias <mschweiz@rhrk.uni-kl.de>
2023-10-21 22:36:36 +0200

    helper functions for convenient plotting

--- src/PyThat/helpers/plotting.py
+++ src/PyThat/helpers/plotting.py
@@ -1,3 +1,3 @@
 from matplotlib.colors import LinearSegmentedColormap
 
-cold_to_hot = LinearSegmentedColormap.from_list('blue_to_red', ['mediumblue', 'darkorchid', 'red'])
+cold_to_hot = LinearSegmentedColormap.from_list('cold_to_hod', ['mediumblue', 'darkorchid', 'red'])
