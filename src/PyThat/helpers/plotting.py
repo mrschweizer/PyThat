20ecb0bf8bc1ecdb82db54c5895cdd827b33e547
Matthias <mschweiz@rhrk.uni-kl.de>
2023-10-21 22:40:27 +0200

    converter from cmaps to cycler

--- src/PyThat/helpers/plotting.py
+++ src/PyThat/helpers/plotting.py
@@ -1,3 +1,9 @@
 from matplotlib.colors import LinearSegmentedColormap
+from cycler import cycler
+from numpy import linspace
 
 cold_to_hot = LinearSegmentedColormap.from_list('cold_to_hod', ['mediumblue', 'darkorchid', 'red'])
+
+def cmap_to_cycler(cmap, n):
+    colors = cmap(linspace(0, 1, n))
+    return cycler(color=colors)
